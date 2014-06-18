85f09a0072e110caa36a81db89d9780e03c18e40
Terry L. Smith <tsmith@tealcomp.com>
2014-06-17 20:21:29 -0400

    earthquakes.py second version

--- earthquakes.py
+++ earthquakes.py
@@ -20,35 +20,74 @@ def str2datetime(timestr):
 def plot_quakes(quakes):
 	""" Plot quakes in passed dataframe """
 
+	# create a set of heatcolor codes
 	heatcolors = ('#FFFF00', '#FF9900', '#CC3333')
-	heatcolor = None
+	heatcolor = None            # initialize heatcolor to None
 
-	cenlat = np.mean(quakes['latitude'])
-	cenlon = np.mean(quakes['longitude'])
-	fig = plt.figure(figsize=(9,9))
-	m = Basemap(resolution = 'l', projection='merc', llcrnrlat = -80, urcrnrlat = 80, llcrnrlon = -180, urcrnrlon = 180, lat_0 = cenlat, lon_0 = cenlon)
-	m.drawcoastlines()
-	m.drawcountries()
-	m.drawstates()
+	cenlat = np.mean(quakes['latitude'])    # find central latitude
+	cenlon = np.mean(quakes['longitude'])   # find central longitude
+	fig = plt.figure(figsize=(9,9))         # create a new figure
+        # create a map using a mercator projection
+	m = Basemap(resolution='l', 
+                    projection='merc',
+                    llcrnrlat=-80, urcrnrlat=80,
+                    llcrnrlon=-180, urcrnrlon=180,
+                    lat_0=cenlat, lon_0=cenlon)
+	m.drawcoastlines()            # add coastlines
+	m.drawcountries()             # add country borders
+	m.drawstates()                # add state borders
+	# fill in countinents and lakes
 	m.fillcontinents(color = '#0CAA43', lake_color = 'aqua')
+	# draw boundary around map
 	m.drawmapboundary(fill_color = '#0B4BD2')
+	# generate x and y values from longitude and latitude
 	x, y = m(quakes['longitude'], quakes['latitude'])
-	
+
+	# loop through x values
 	for i in range(len(x)):
 		if quakes['depth'][i:i+1].any() < 70:
+			# set color for depths less than 70
 			heatcolor = heatcolors[1]
 		elif 70 <= quakes['depth'][i:i+1].any() < 300:
+			# set color for depths between 70 and 300
 			heatcolor = heatcolors[2]
 		else:
+			# set color for depths greater than or equal to 300
 			heatcolor = heatcolors[3]
+		# plot quakes using circles with colors appropriate for
+		# depth and radius relative to magnitude
 		m.plot(x[i:i+1], y[i:i+1], heatcolor, marker = 'o', markersize = (np.pi * quakes['mag'][i:i+1]**2)/2, alpha = 0.6)
 	fig.show()
 
-def plot_quake_mag_by_time(quakes):
-	""" plot earthquake magnitudes by time """
-	x, y = str2datetime(quakes['time']), quakes['mag']
 
-	plt.plot(x,y)
+def plot_quake_counts(quakes):
+	""" plot earthquake counts by day as histogram """
+	dates = []                    # initialize list of dates
+	quake_counts = []             # initialize list for quake counts
+	this_date = date_min.date()   # initialize date iterator
+	# loop through dates until all dates have been examined
+	while this_date <= date_max.date():
+		cnt = 0               # initialize quake counter
+		# loop through quake data, looking for dates
+		# matching the current date
+		for tm in eq['time']:
+			if this_date == str2datetime(tm).date():
+				cnt = cnt + 1
+		# store the date and daily counts in lists
+		dates.append(this_date)
+		quake_counts.append(cnt)
+		this_date = this_date + datetime.timedelta(days=1)
+	# plot quake counts as a series of horizontal lines
+	plt.hlines(dates, 
+		   0, quake_counts,
+		   colors='b',
+		   linestyles='solid')
+	plt.title('Daily Quake Counts')
+	plt.xlabel('Number of Quakes')
+	plt.ylabel('Date')
+	plt.grid(True)
+	plt.show()
+
 
 def quake_feature_stats(quakes, feature):
 	""" Return dictionary containing mininum ('min'), maximum ('max'), mean, and standard deviation ('std') for the specified feature """
@@ -59,8 +98,9 @@ def quake_feature_stats(quakes, feature):
 		'std' : np.std(quakes[feature])}
 
 
-# --- read data ---
-eq = pd.read_csv('usgs_earthquakes_all_20140327.csv', sep=',', header=0)
+# --- main routine ---
+
+eq = pd.read_csv('usgs_earthquakes_all_20140407.csv', sep=',', header=0)
 
 # describe the data set
 date_min = np.min([str2datetime(tm) for tm in eq['time']])
@@ -84,26 +124,15 @@ print('{0}\t{1:0.1f}\t{2:0.1f}\t{3:0.1f}\t{4:0.1f}'.format(
 	stats_mag['mean'],
 	stats_mag['std']))
 
+
 # pause for user
 print('')
-junk = raw_input('Press ENTER to examine daily quake counts; else press CTRL-C')
+junk = raw_input('Press ENTER to plot daily quake counts; else press CTRL-C')
 
-# examine daily quake counts
-quake_counts = []
-#num_days = (date_max - date_min).days
-this_date = date_min.date()
-while this_date <= date_max.date():
-	cnt = 0
-	for tm in eq['time']:
-		eqdate = str2datetime(tm).date()
-		if this_date == eqdate:
-			cnt = cnt + 1
-	quake_counts.append((this_date, cnt))
-	this_date = this_date + datetime.timedelta(days=1)
-for item in quake_counts:
-	print('{0}: {1}'.format(item[0].isoformat(), item[1]))
-
-
+# generate plot of daily quake counts
+print('\nThis may take a moment ...')
+plot_quake_counts(eq)
+print('The plot is complete.  Take a look!')
 
 # pause for user
 print('')
@@ -114,45 +143,7 @@ print('\nThis may take a moment ...')
 plot_quakes(eq)
 print('The plot is complete.  Take a look!')
 
-# select desired subset, say those quakes with magnitudes greater than
-# the mean
-#mean_mag = np.mean(eq['mag'])
-
-#print('Quakes in this 30-day period had a mean magnitude of {0:0.2f} with a standard deviation of {1:0.2f}'.format(mean_mag))
-#my_quakes = eq[eq['mag'] > mean_mag]
-#my_times = [tm for tm in my_quakes['time']]
-#my_lats = [lat for lat in my_quakes['latitude']]
-#my_lons = [lon for lon in my_quakes['longitude']]
-#my_depths = [depth for depth in my_quakes['depth']]
-#my_mags = [mag for mag in my_quakes['mag']]
-
-#print('The following quakes had magnitudes greater than the mean for this 30-day period:')
-#print('Time\tMagnitude\tLatitude\tLongitude\tDepth (m)')
-#for i in range(len(my_times)):
-#	print('{0}\t{1:0.2f}\t{2:0.6f}\t{3:0.6f}\t{4:0.1f}'.format(
-#		my_times[i],
-#		my_mags[i],
-#		my_lats[i],
-#		my_lons[i],
-#		my_depths[i]))
-
-
-
-
-#events = []
-#for i in range(len(eq)):
-#	dt = str2datetime(eq['time'][i])
-#	lat = eq['latitude'][i]
-#	lon = eq['longitude'][i]
-#	depth = eq['depth'][i]
-#	mag = eq['mag'][i]
-#
-#	events.append((dt, mag))
-#
-#plt.plot(events[0], events[1], 'b+')
-#	timenow = datetime.datetime.now()
-#	since = (timenow - dt)
-#	print('{0} hours before now'.format(since.seconds / 3600))
-
-#	print('{0}:{1}:{2}:{3}:{4}'.format(since, lat, lon, depth, mag))
+# pause for user
+print('')
+junk = raw_input('All done ... press ENTER')
 
